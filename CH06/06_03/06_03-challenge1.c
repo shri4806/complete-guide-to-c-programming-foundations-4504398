27015266068248ea57d6ee51e96741fb99154a2f
shri4806 <shrithareddyloka@gmail.com>
2024-12-27 17:19:48 +0530

    Update 06_03-challenge1.c

--- CH06/06_03/06_03-challenge1.c
+++ CH06/06_03/06_03-challenge1.c
@@ -2,5 +2,18 @@
 
 int main()
 {
+	char string[]= "yes MADAM, i Agree"
+	int ch;
+	int x=0;
+
+	while(ch(string)!='\0'){
+		if(isupper(ch))
+			putchar(islower(ch));
+		else if(islower(ch))
+			putchar(isupper(ch));
+		else
+				putchar(ch);
+	x++;
+	}
 	return 0;
 }
